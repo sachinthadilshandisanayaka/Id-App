3b83d1dcd238513caa1889ea5a36c1d0d03604c7
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-10-08 14:31:48 +0530

    create flutter id app

---
+++ android/app/src/main/kotlin/com/example/idapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.idapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+// import 'dart:html';
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: IdApp(),
+  ));
+}
+
+class IdApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //this 'Scaffold' help to make quickly layout for our app, icon bar,app bar
+      appBar: AppBar(
+        title: Text('My Id Card'),
+        centerTitle: true,
+        backgroundColor: Colors.teal[900],
+        elevation: 0.0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(10.0, 20.0, 10.0, 0.0),
+        child: Column(
+          children: <Widget>[
+            Text(
+              'NAME',
+              style: TextStyle(
+                color: Colors.amberAccent[300],
+                letterSpacing: 2.0,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
