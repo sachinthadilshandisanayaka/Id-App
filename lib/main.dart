bde790b34e3ca91e8deed0f733f38913364f4592
sachinthadilshandisanayaka <sachidisanayaka1998@gmail.com>
2020-10-08 18:34:31 +0530

    finnished idapp

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 // import 'dart:html';
+// import 'dart:html';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
@@ -14,16 +15,29 @@ class IdApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       //this 'Scaffold' help to make quickly layout for our app, icon bar,app bar
+      backgroundColor: Color.fromRGBO(22, 160, 133, 1.0),
       appBar: AppBar(
         title: Text('My Id Card'),
         centerTitle: true,
-        backgroundColor: Colors.teal[900],
-        elevation: 0.0,
+        backgroundColor: Color.fromRGBO(26, 188, 156, 1.0),
+        elevation: 10.0, // box shadow
       ),
       body: Padding(
         padding: EdgeInsets.fromLTRB(10.0, 20.0, 10.0, 0.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/a.jpg'),
+                radius: 45.0,
+              ),
+            ),
+            Divider(
+              // this use to set defferece each widgets
+              height: 100.0,
+              color: Color.fromRGBO(44, 62, 80, 1.0),
+            ),
             Text(
               'NAME',
               style: TextStyle(
@@ -31,13 +45,61 @@ class IdApp extends StatelessWidget {
                 letterSpacing: 2.0,
               ),
             ),
+            SizedBox(
+              height: 10.0, // space between two widget
+            ),
             Text(
               'Sachintha Dilshan',
               style: TextStyle(
                 color: Colors.white,
                 letterSpacing: 1.0,
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
               ),
-            )
+            ),
+            SizedBox(
+              height: 30.0, // space between each widget
+            ),
+            Text(
+              'AGE',
+              style: TextStyle(
+                color: Colors.amberAccent[300],
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(
+              height: 10.0, // space between two widget
+            ),
+            Text(
+              '21',
+              style: TextStyle(
+                color: Colors.white,
+                letterSpacing: 1.0,
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Icon(Icons.email),
+                SizedBox(
+                  width: 10.0,
+                ),
+                Text(
+                  'sachidisanayaka1998@gmail.com',
+                  style: TextStyle(
+                    color: Colors.white,
+                    letterSpacing: 1.0,
+                    fontSize: 17.5,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
       ),
